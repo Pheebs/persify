4b5f5075879f95cdc436e66b3a5fde688e5ce916
Hamed Valizadeh <havaliza@gmail.com>
2017-12-04 12:49:17 +0330

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+(function(name, definition) {
+  if (typeof define === 'function') {
+    // AMD
+    define(definition);
+  } else if (typeof module !== 'undefined' && module.exports) {
+    // Node.js
+    module.exports = definition();
+  } else {
+    // Browser
+    var theModule = definition(),
+      global = this,
+      old = global[name];
+    theModule.noConflict = function() {
+      global[name] = old;
+      return theModule;
+    };
+    global[name] = theModule;
+  }
+})('persify', function() {
+  return function(str) {
+    return str.replace(/\d+/g, function(digit) {
+      var ret = '';
+      for (var i = 0, len = digit.length; i < len; i++) {
+        ret += String.fromCharCode(digit.charCodeAt(i) + 1728);
+      }
+      return ret;
+    });
+  };
+});
